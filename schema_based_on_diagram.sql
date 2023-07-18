9dcb128976aba7635236ba762d87bd7c28c5d698
gersonhiggins <gahg.10gerson@gmail.com>
2023-07-18 15:55:46 -0400

    Fix requirement changes

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -9,20 +9,25 @@ CREATE TABLE medical_histories (
   admitted_at       TIMESTAMP,
   patient_id        INT REFERENCES patients(id),
   status            VARCHAR
-)
-;
+);
+
+CREATE TABLE treatments (
+  id                SERIAL PRIMARY KEY,
+  type              VARCHAR,
+  name              VARCHAR
+);
+
+CREATE TABLE medical_histories_has_treatments (
+  medical_history_id INT REFERENCES medical_histories(id),
+  treatment_id INT REFERENCES treatments(id)
+);
+
 CREATE TABLE invoices (
   id                SERIAL PRIMARY KEY,
   total_amount      DECIMAL,
   generated_at      TIMESTAMP,
   payed_at          TIMESTAMP,
   medical_history_id INT REFERENCES medical_histories(id)
-);
-
-CREATE TABLE treatments (
-  id                INT PRIMARY KEY REFERENCES medical_histories(id),
-  type              VARCHAR,
-  name              VARCHAR,
 );
 
 CREATE TABLE invoice_items (
