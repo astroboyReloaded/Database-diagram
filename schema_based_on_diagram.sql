0a30cdc3cbd57fe9b3af7a7a4f3939c7bada2544
AstroboyReloaded <astroboy.reloaded@gmail.com>
2023-07-18 11:23:25 -0600

    Diagram to Schema

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,35 @@
+CREATE TABLE patients (
+  id                SERIAL PRIMARY KEY,
+  name              VARCHAR,
+  date_of_birth     DATE
+);
+
+CREATE TABLE medical_histories (
+  id                SERIAL PRIMARY KEY,
+  admitted_at       TIMESTAMP,
+  patient_id        INT REFERENCES patients(id),
+  status            VARCHAR
+)
+;
+CREATE TABLE invoices (
+  id                SERIAL PRIMARY KEY,
+  total_amount      DECIMAL,
+  generated_at      TIMESTAMP,
+  payed_at          TIMESTAMP,
+  medical_history_id INT REFERENCES medical_histories(id)
+);
+
+CREATE TABLE treatments (
+  id                INT REFERENCES medical_histories(id),
+  type              VARCHAR,
+  name              VARCHAR,
+);
+
+CREATE TABLE invoice_items (
+  id                SERIAL PRIMARY KEY,
+  unit_price        DECIMAL,
+  quantity          INT,
+  total_price       DECIMAL,
+  invoice_id        INT REFERENCES invoices(id),
+  treatment_id      INT REFERENCES treatments(id)
+);
